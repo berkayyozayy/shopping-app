b8290e2364c9fbc708c5103074c19ff64675db85
Berkay <berkay@Berkay-MacBook-Air.local>
2022-11-18 17:22:52 +0300

    feat: add filters component and display the category name

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+})
+export class FiltersComponent implements OnInit {
+  @Output() showCategory = new EventEmitter<string>()
+  categories = ['electronic', 'spor']
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category)
+  }
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string | undefined;
 
   constructor() { }
 
@@ -14,5 +15,8 @@ export class HomeComponent implements OnInit {
   
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
+  }
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
