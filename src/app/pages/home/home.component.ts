36f7d720d4c4374e41846cc9a5be822880e04c69
Berkay <berkay@Berkay-MacBook-Air.local>
2022-11-20 17:17:19 +0300

    feat: implement product box component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const ROWS_HEIGHT: {[id: number]: number} = {1: 400, 2: 355, 3: 350}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols]
   category: string | undefined;
 
   constructor() { }
@@ -15,6 +19,9 @@ export class HomeComponent implements OnInit {
   
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols]
+    console.log('rowHeight ',this.rowHeight);
+    
   }
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
