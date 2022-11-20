3e7d7bd9c56a6953490e2eb1f78d235ab512fe6c
Berkay <berkay@Berkay-MacBook-Air.local>
2022-11-20 21:33:28 +0300

    feat(home-product-box): add item to cart

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,12 +8,22 @@ import { Component, Input, OnInit } from '@angular/core';
   
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
+  @Output() addToCart = new EventEmitter()
+  product: Product | undefined = {
+    id: 1,
+    price: 200,
+    title: 'blabla',
+    category: 'electronic',
+    description: 'Desc',
+    image: 'https://via.placeholder.com/200.'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  
-
+  onAddToCart(): void {
+    this.addToCart.emit(this.product)
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 
 const ROWS_HEIGHT: {[id: number]: number} = {1: 400, 2: 355, 3: 350}
@@ -12,7 +14,7 @@ export class HomeComponent implements OnInit {
   rowHeight = ROWS_HEIGHT[this.cols]
   category: string | undefined;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
   }
@@ -21,9 +23,17 @@ export class HomeComponent implements OnInit {
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[this.cols]
     console.log('rowHeight ',this.rowHeight);
-    
   }
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+  }
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart({
+      id: product.id,
+      price: product.price,
+      product: product.image,
+      name: product.title,
+      quantity: 1,
+    })
   }
 }
